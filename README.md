a0338befa59cc3a575faa5778668cce8c41d71c1
Tobias Alexander Franke <tobias.franke@siggraph.org>
2023-08-26 00:39:03 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,54 @@
+# Tzeentch
+A load-balancing redirector for [privacy-oriented alternative frontends](https://github.com/mendel5/alternative-front-ends). Inspired and partially rewritten from [Farside.link](https://github.com/benbusby/farside).
+
+Demo instance at [https://alternator.hstn.me](https://alternator.hstn.me).
+
+## About
+**Tzeentch** maintains a list of links to [privacy-oriented alternative frontends](https://github.com/mendel5/alternative-front-ends), which scrape popular but horribly written websites and present them in a much cleaner, ad- and tracking-free way. 
+
+Alternative frontends can easily get rate-limited or banned if there is an unusual amount of traffic coming from them because too many users are on a single instance. In order to help distribute users more effectively, **Tzeentch** will randomly select an instance and forward the original request to it.
+
+To use it, you will need to set up a redirector add-on for your browser such as [Redirector](https://addons.mozilla.org/en-US/firefox/addon/redirector/) or [Redirect Web for Safari](https://apps.apple.com/us/app/redirect-web-for-safari/id1571283503). 
+
+As an example, instead of redirecting `reddit.com` to a `libreddit` instance, you instead redirect it to `https://tzeentch-instance/?libreddit/r/subreddit`.
+
+## Difference to LibRedirect
+[LibRedirect](https://libredirect.github.io/) is a browser add-on that already does most of the work here without the need for another redirector add-on and a webservice like this. However, it does not work on mobile devices (there is a hack for Android, but none for iOS) and it has a fixed scope. In contrast, with **Tzeentch** you can create your own list of frontends and their respective list of instances as soon as you discover them, and simply add them directly to your favorite redirector add-on.
+
+## Difference to Farside.link
+[Farside.link](https://github.com/benbusby/farside) was the inspiration to create **Tzeentch**. The reason for this clone (instead of contributing to Farside) is twofold:
+1. I do not know any Erlang/Elexir, the language Farside is written in.
+2. I cannot install the dependencies of Farside on my VPS.
+
+**Tzeentch** is written in PHP, which is universally available, and even free hosts with PHP support exist, making it much easier to deploy. You will only need to upload a single file `index.php` to run **Tzeentch** on a VPS, and an additional `data.json` if you want to maintain a local configuration for your own frontends and their instances.
+
+## Configuration
+Every alternative frontend and its instances are stored in a JSON configuration which **Tzeentch** refers to. The configuration is compatible with that of [LibRedirect](https://libredirect.github.io/).
+
+**Tzeentch** will search for configurations in this order:
+1. A local JSON file called `data.json`
+2. [The latest commited configuration in the Tzeentch repository](https://github.com/thefranke/Tzeentch/blob/main/data.json)
+2. [The latest commited configuration of LibRedirect](https://github.com/libredirect/instances/blob/main/data.json)
+3. [The latest commited configuration of Farside](https://github.com/benbusby/farside/blob/main/services-full.json)
+
+If you want to add a new or custom frontend, fetch a copy of `data.json` from this repository and simply add the frontend in question as follows:
+
+```
+"alternative-frontend-name": {
+    "clearnet": [
+      "url1",
+      "url2"
+    ],
+    "tor": [ ... ],
+    "i2p": [ ... ],
+    "loki": [ ... ]
+  }
+```
+
+## Future enhancements
+
+* Tzeentch currently only supplies clearnet links and should later have options for to supply Tor, non-cloudflare and other types of URLs
+* There are probably unhandled exceptions
+* Check instance health
+* Autodetect frontends from full urls
+* Multi-frontend forwarding support (e.g. Teddit and LibReddit are both frontends for Reddit)
---
+++ index.php
@@ -0,0 +1,208 @@
+<?php
+
+/*
+ * Tzeentch - A load-balancing redirector for privacy-oriented alternative frontends. 
+ * 
+ * https://github.com/thefranke/tzeentch
+ *
+ */
+
+$params = "";
+$last_updated = "";
+$config = "";
+
+function fetch_param()
+{
+    $keys = array_keys($_GET);
+    $p = "";
+    if (isset($keys[0]))
+        $p = $keys[0];
+
+    return $p;
+}
+
+function fetch_json($path)
+{
+    $opts = [
+        'http' => [
+                'method' => 'GET',
+                'header' => [
+                        'User-Agent: PHP',
+                ]
+        ]
+    ];
+
+    $context = stream_context_create($opts);
+    $json_raw = @file_get_contents($path, false, $context);
+    return json_decode($json_raw, true);
+}
+
+function fetch_gh_timestamp($repo, $file)
+{
+    $url = "https://api.github.com/repos/$repo/commits?path=$file";
+    $json = fetch_json($url);
+    return $json[0]["commit"]["author"]["date"];
+}
+
+function fetch_gh_content($repo, $file)
+{
+    $url = "https://raw.github.com/$repo/main/$file";
+    $json = fetch_json($url);
+    return $json;
+}
+
+function load_config()
+{
+    global $config;
+    global $last_updated;
+
+    // check for local configuration, early out
+    $local_config = "data.json";
+    if (file_exists($local_config))
+    {
+        $config = fetch_json($local_config);
+        $last_updated = "Using local synced " . date ("Y-m-d H:i:s T", filemtime($local_config));
+        return;
+    }
+
+    $repos = [
+        ["thefranke/tzeentch", "data.json"],
+        ["libredirect/instances", "data.json"],
+        ["benbusby/farside", "services-full.json"]
+    ];
+
+    foreach($repos as $repo)
+    {
+        if ($config)
+            break;
+
+        $config = fetch_gh_content($repo[0], $repo[1]);
+        
+        if ($config) 
+        {
+            $last_updated = "Using " . $repo[0] . " synced " . fetch_gh_timestamp($repo[0], $repo[1]);
+        
+            // convert to libredirect format
+            if ($repo[0] == "benbusby/farside")
+            {
+                $new_config = array();
+                foreach ($config as $service)
+                {
+                    $type = $service["type"];
+                    $instances = $service["instances"];
+                    $new_config[$type] = array();
+                    $new_config[$type]["clearnet"] = $instances;
+                }
+                $config = $new_config;
+            }
+        }
+    }
+}
+
+function print_frontpage($config)
+{
+    if (!$config)
+    {
+        echo "Error: Services configuration could not be loaded";
+        return;
+    }
+
+    foreach($config as $service => $instances)
+    {
+        $service = strtolower($service);
+
+        echo "<li><a href=\"?" . $service . "\">" . $service . "</a></li>\n";
+        echo "<ul>\n";
+        
+        foreach ($instances["clearnet"] as $instance)
+        {
+            $instance = explode("|", $instance);
+            echo "<li><a href=\"" . $instance[0] . "\">" . $instance[0] . "</a></li>\n";
+        }
+
+        echo "</ul>\n";
+    }
+}
+
+function forward_to_random_instance($config, $param)
+{
+    $params = explode("/", $param, 2);
+    $frontend = $params[0];
+    $frontend_param = implode(array_slice($params, 1));
+
+    foreach($config as $service => $instances)
+    {
+        $service = strtolower($service);
+        if($frontend != $service)
+            continue;
+
+        $k = array_rand($instances["clearnet"], 1);
+        $random_instance = $instances["clearnet"][$k];
+        die(header('Location: '.$random_instance . "/" . $frontend_param));
+    }
+}
+
+$param = fetch_param();
+load_config();
+
+if($param)
+    forward_to_random_instance($config, $param);
+
+?>
+<html>
+
+<head>
+  <title>Tzeentch</title>
+  <style>
+    html {
+      font-family: monospace;
+      font-size: 16px;
+      color: #66397C;
+      text-align: center;
+    }
+    #main {
+      text-align: left;
+      margin: auto;
+      min-width: 500px;
+      display: inline-block;
+    }
+    hr {
+      border: 1px dashed;
+    }
+    a:link, a:visited {
+      color: #66397C;
+    }
+    ul {
+      margin: 10px;
+    }
+    #header {
+        text-align: center;
+        margin: 40pt;
+    }
+    h1, h2, h3, h4 {
+        margin: 5pt;
+    }
+    img {
+        width: 120pt;
+    }
+  </style>
+</head>
+
+<body>
+    <div id="main">
+        <div id="header">
+            <img src="https://bakadesign.dk/backoffice/wp-content/uploads/2019/10/Star-of-Chaos-02.svg">
+
+            <h1>Tzeentch</h1>
+            <h3>"Changer of Ways, Great Mutator, Lord of Entropy"</h3>
+            <h4>[<a href="https://github.com/thefranke/tzeentch">Github</a>]</h4>
+            <hr>
+            <h4><?php echo $last_updated; ?></h3>
+        </div>
+        <ul>
+            <?php print_frontpage($config); ?>
+        </ul>
+    </div>
+</body>
+
+</html>
